8227d7b1bc382343dcbc9a447dbf59cb6201b97e
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-09-27 03:29:40 -0300

    Added new font and improved footer background

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,13 @@
 
 <template>
     <div class="bg_footer">
+        <div class="custom-shape-divider-bottom-1695792723">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+            </svg>
+        </div>
         <div class="social_img__wrapper">
             <a href="https://github.com/L03A95" target="_blank"><img src="../images/github_logo.png" class="social_img"></a>
             <a href="https://www.linkedin.com/in/liam-perez-lupia-33a189257/" target="_blank"><img src="../images/linkedin_logo.png" class="social_img"></a> 
@@ -23,16 +30,38 @@
 </template>
 
 <style>
+    .custom-shape-divider-bottom-1695792723 {
+    position: absolute;
+    top: -120px;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1695792723 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 120px;
+}
+
+.custom-shape-divider-bottom-1695792723 .shape-fill {
+    fill: #00DFC0;
+}
+
     .a_btn {
         width: 90%;
     }
     .bg_footer {
         height: 30vh;
-        background: #1f1a1a;
+        background: #00DFC0;
         padding-top: 5vh;
         display: flex;
         justify-content: space-around;
         flex-wrap: wrap;
+        position: relative;
     }
 
     .social_img {
@@ -44,8 +73,8 @@
 
     .social_text {
         color:#4f4a4a ;
-        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size: 14px;
+        font-family: 'Poppins';
+        font-size: 18px;
         display: inline-block;
     }
 
@@ -80,7 +109,7 @@
 
     @media screen and (min-width: 1024px) { 
         .social_text {
-            font-size: 18px;
+            font-size: 16px;
         }
     }
 </style>
